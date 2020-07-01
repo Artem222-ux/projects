021a9c95b21383f2658bd423e48fa52ce7bfeaa6
Artem222-ux <66218608+Artem222-ux@users.noreply.github.com>
2020-07-01 19:13:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,379 @@
+$('#q').click(function(){
+	$('#hm').css("background-color", "rgb(0,100,9)");
+
+})
+$('#w').click(function(){
+	$('#hm').css("background-color", "lime");
+
+})
+$('#e').click(function(){
+	$('#hm').css("background-color", "rgb(255,83,12)");
+
+})
+$('#r').click(function(){
+	$('#hm').css("background-color", "yellow");
+
+})
+$('#t').click(function(){
+	$('#hm').css("background-color", "rgb(101,9,9)");
+
+})
+$('#y').click(function(){
+	$('#hm').css("background-color", "rgb(222,0,0)");
+
+})
+$('#u').click(function(){
+	$('#hm').css("background-color", "rgb(240,47,106)");
+
+})
+$('#i').click(function(){
+	$('#hm').css("background-color", "rgb(250,109,167)");
+
+})
+$('#o').click(function(){
+	$('#hm').css("background-color", "rgb(90,56,41)");
+
+})
+$('#p').click(function(){
+	$('#hm').css("background-color", "rgb(105,62,219)");
+
+})
+$('#a').click(function(){
+	$('#hm').css("background-color", "rgb(0,47,124)");
+
+})
+$('#s').click(function(){
+	$('#hm').css("background-color", "rgb(3,9,44)");
+})
+$('#d').click(function(){
+	$('#hm').css("background-color", "rgb(0,123,183)");
+
+})
+$('#f').click(function(){
+	$('#hm').css("background-color", "rgb(0,154,145)");
+
+})
+$('#g').click(function(){
+	$('#hm').css("background-color", "RGB(86,157,151)");
+
+})
+$('#h').click(function(){
+	$('#hm').css("background-color", "rgb(163,163,163)");
+
+})
+$('#j').click(function(){
+	$('#hm').css("background-color", "rgb(249,222,195)");
+
+})
+$('#k').click(function(){
+	$('#hm').css("background-color", "rgb(0,0,0)");
+
+})
+$('#l').click(function(){
+	$('#hm').css("background-color", "RGB(186,159,138) ");
+
+})
+$('#z').click(function(){
+	$('#hm').css("background-color", "RGB(196,159,179)");
+
+})
+$('#x').click(function(){
+	$('#hm').css("background-color", "rgb(241,183,208)");
+
+})
+$('#c').click(function(){
+	$('#hm').css("background-color", "RGB(10,30,31)");
+
+})
+
+//mat
+
+
+$('#black-m').click(function(){
+	$('#hm').css("background-color", "rgb(0,100,9)");
+
+})
+$('#orange-m').click(function(){
+	$('#hm').css("background-color", "lime");
+
+})
+$('#yellow-m').click(function(){
+	$('#hm').css("background-color", "rgb(255,83,12)");
+
+})
+$('#grey-m').click(function(){
+	$('#hm').css("background-color", "yellow");
+
+})
+$('#silver-m').click(function(){
+	$('#hm').css("background-color", "rgb(84,6,6)");
+
+})
+$('#gold-m').click(function(){
+	$('#hm').css("background-color", "rgb(222,0,0)");
+
+})
+$('#lime-m').click(function(){
+	$('#hm').css("background-color", "rgb(240,47,106)");
+
+})
+$('#pink-m').click(function(){
+	$('#hm').css("background-color", "rgb(250,109,167)");
+
+})
+$('#brown-m').click(function(){
+	$('#hm').css("background-color", "rgb(90,56,41)");
+
+})
+$('#violet-m').click(function(){
+	$('#hm').css("background-color", "rgb(105,62,219)");
+
+})
+$('#red-m').click(function(){
+	$('#hm').css("background-color", "rgb(0,47,124)");
+
+})
+$('#darkBlue-m').click(function(){
+	$('#hm').css("background-color", "rgb(3,9,43)");
+
+})
+$('#blue-m').click(function(){
+	$('#hm').css("background-color", "rgb(0,154,145)");
+
+})
+$('#LightSkyBlue-m').click(function(){
+	$('#hm').css("background-color", "rgb(0,123,183)");
+
+})
+$('#SteelBlue-m').click(function(){
+	$('#hm').css("background-color", "RGB(86,157,151)");
+
+})
+$('#coral-m').click(function(){
+	$('#hm').css("background-color", "rgb(163,163,163)");
+
+})
+$('#Crimson-m').click(function(){
+	$('#hm').css("background-color", "rgb(249,222,195)");
+
+})
+$('#GreenYellow-m').click(function(){
+	$('#hm').css("background-color", "rgb(0,0,0)");
+
+})
+$('#Aquamarine-m').click(function(){
+	$('#hm').css("background-color", "RGB(186,159,138)");
+
+})
+$('#PapayaWhip-m').click(function(){
+	$('#hm').css("background-color", "RGB(196,159,179)");
+
+})
+$('#indigo-m').click(function(){
+	$('#hm').css("background-color", "rgb(241,183,208)");
+
+})
+$('#green-m').click(function(){
+	$('#hm').css("background-color", "RGB(10,30,31)");
+
+})
+
+//png
+
+
+$('#q').click(function(){
+	$('.png').attr('src', 'https://i5.stat01.com/2/2378/123776268/075a3e/naushniki-airpods-2-color-abyss-glyanec-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+$('#w').click(function(){
+	$('.png').attr('src', 'https://i1.stat01.com/2/2379/123781528/075a3e/naushniki-airpods-2-color-yellow-glyanec-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+$('#e').click(function(){
+	$('.png').attr('src', 'https://i5.stat01.com/2/2380/123792391/075a3e/naushniki-airpods-2-color-cranberry-glyanec-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+$('#r').click(function(){
+	$('.png').attr('src', 'https://i5.stat01.com/2/2379/123780113/075a3e/naushniki-airpods-2-color-caribbian-glyanec-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+$('#t').click(function(){
+	$('.png').attr('src', 'https://i4.stat01.com/2/2380/123792358/075a3e/naushniki-airpods-2-color-concord-glyanec-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+$('#y').click(function(){
+	$('.png').attr('src', 'https://i5.stat01.com/2/2380/123794442/075a3e/naushniki-airpods-2-color-pink-glyanec-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+$('#u').click(function(){
+	$('.png').attr('src', 'https://i5.stat01.com/2/2380/123794807/075a3e/naushniki-airpods-2-color-grey-glyanec-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+$('#i').click(function(){
+	$('.png').attr('src', 'https://i5.stat01.com/2/2380/123792725/075a3e/naushniki-airpods-2-color-orange-glyanec-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+$('#o').click(function(){
+	$('.png').attr('src', 'https://i4.stat01.com/2/2380/123792107/075a3e/naushniki-airpods-2-color-cobalt-glyanec-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+$('#p').click(function(){
+	$('.png').attr('src', 'https://i5.stat01.com/2/2380/123792253/075a3e/naushniki-airpods-2-color-coffe-glyanec-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+$('#a').click(function(){
+	$('.png').attr('src', 'https://i1.stat01.com/2/2379/123789911/075a3e/naushniki-airpods-2-color-clover-glyanec-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+$('#s').click(function(){
+	$('.png').attr('src', 'https://i4.stat01.com/2/2378/123776092/075a3e/naushniki-airpods-2-color-black-glyanec-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+$('#d').click(function(){
+	$('.png').attr('src', 'https://i4.stat01.com/2/2380/123794978/075a3e/naushniki-airpods-2-color-fusion-glyanec-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+$('#f').click(function(){
+	$('.png').attr('src', 'https://i2.stat01.com/2/2378/123776523/075a3e/naushniki-airpods-2-color-billiard-glyanec-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+$('#g').click(function(){
+	$('.png').attr('src', 'https://www.icases.ru/upload/iblock/999/9991e7696e94ed7646de0760c4c4c9ff.jpg');
+});
+
+$('#h').click(function(){
+	$('.png').attr('src', 'https://i1.stat01.com/2/2380/123794590/075a3e/naushniki-airpods-2-color-red-glyanec-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+$('#j').click(function(){
+	$('.png').attr('src', 'https://i4.stat01.com/2/2380/123795080/075a3e/naushniki-airpods-2-color-paradise-glyanec-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+$('#k').click(function(){
+	$('.png').attr('src', 'https://i5.stat01.com/2/2359/123589995/075a3e/jetblack-glyancevyj-png.png');
+});
+$('#l').click(function(){
+	$('.png').attr('src', 'https://www.icases.ru/upload/iblock/2ff/2ffbbea44f4573657586aa26bc4ea4cf.jpg');
+});
+
+$('#z').click(function(){
+	$('.png').attr('src', 'https://www.icases.ru/upload/iblock/fb0/fb04c70ec41a828e25cc42b2ad96ea3c.jpg');
+});
+
+$('#x').click(function(){
+	$('.png').attr('src', 'https://www.icases.ru/upload/iblock/fb0/fb04c70ec41a828e25cc42b2ad96ea3c.jpg');
+});
+
+$('#c').click(function(){
+	$('.png').attr('src', 'https://touch.com.ua/upload/iblock/4a4/4a456e93c65e6fe9042b0f34e1d29504.jpg');
+});
+
+//matoviy
+
+$('#black-m').click(function(){
+	$('.png').attr('src', 'https://i2.stat01.com/2/2378/123776306/075a3e/naushniki-airpods-2-color-abyss-matovye-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+
+$('#orange-m').click(function(){
+	$('.png').attr('src', 'https://i1.stat01.com/2/2379/123781556/075a3e/naushniki-airpods-2-color-yellow-matovye-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+
+$('#yellow-m').click(function(){
+	$('.png').attr('src', 'https://i5.stat01.com/2/2380/123792397/075a3e/naushniki-airpods-2-color-cranberry-matovye-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+
+$('#grey-m').click(function(){
+	$('.png').attr('src', 'https://i5.stat01.com/2/2379/123780269/075a3e/naushniki-airpods-2-color-caribbian-matovye-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+
+$('#silver-m').click(function(){
+	$('.png').attr('src', 'https://i5.stat01.com/2/2380/123792372/075a3e/naushniki-airpods-2-color-concord-matovye-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+
+$('#gold-m').click(function(){
+	$('.png').attr('src', 'https://i5.stat01.com/2/2380/123794444/075a3e/naushniki-airpods-2-color-pink-matovye-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+
+$('#lime-m').click(function(){
+	$('.png').attr('src', 'https://i4.stat01.com/2/2380/123794819/075a3e/naushniki-airpods-2-color-grey-matovye-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+
+$('#pink-m').click(function(){
+	$('.png').attr('src', 'https://i1.stat01.com/2/2380/123792835/075a3e/naushniki-airpods-2-color-orange-matovye-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+
+$('#brown-m').click(function(){
+	$('.png').attr('src', 'https://i5.stat01.com/2/2380/123792124/075a3e/naushniki-airpods-2-color-cobalt-matovye-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+
+$('#violet-m').click(function(){
+	$('.png').attr('src', 'https://i5.stat01.com/2/2380/123792306/075a3e/naushniki-airpods-2-color-coffe-matovye-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+
+$('#red-m').click(function(){
+	$('.png').attr('src', 'https://i1.stat01.com/2/2379/123789984/075a3e/naushniki-airpods-2-color-clover-matovye-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+
+$('#darkBlue-m').click(function(){
+	$('.png').attr('src', 'https://i1.stat01.com/2/2378/123776184/075a3e/naushniki-airpods-2-color-black-matovye-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+
+$('#LightSkyBlue-m').click(function(){
+	$('.png').attr('src', 'https://i4.stat01.com/2/2380/123794985/075a3e/naushniki-airpods-2-color-fusion-matovye-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+
+$('#blue-m').click(function(){
+	$('.png').attr('src', 'https://i5.stat01.com/2/2378/123776663/075a3e/naushniki-airpods-2-color-billiard-matovye-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+
+$('#SteelBlue-m').click(function(){
+	$('.png').attr('src', 'https://www.icases.ru/upload/iblock/c21/c215600fcdc98eab0b058a4affd4f96b.jpg');
+});
+
+
+$('#coral-m').click(function(){
+	$('.png').attr('src', 'https://i5.stat01.com/2/2380/123794694/075a3e/naushniki-airpods-2-color-red-matovye-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+
+$('#Crimson-m').click(function(){
+	$('.png').attr('src', 'https://i4.stat01.com/2/2380/123795084/075a3e/naushniki-airpods-2-color-paradise-matovye-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+
+$('#GreenYellow-m').click(function(){
+	$('.png').attr('src', 'https://i4.stat01.com/2/2378/123774344/075a3e/naushniki-airpods-2-color-black-glyanec-bez-besprovodnoj-zaryadki-chehla.png');
+});
+
+$('#Aquamarine-m').click(function(){
+	$('.png').attr('src', 'https://www.icases.ru/upload/iblock/955/955aa1bddd24dee7359e2834f15845f7.jpg');
+});
+
+
+$('#PapayaWhip-m').click(function(){
+	$('.png').attr('src', 'https://www.icases.ru/upload/iblock/e64/e648b4836083b74445ce81169b209968.jpg');
+});
+
+
+$('#indigo-m').click(function(){
+	$('.png').attr('src', 'https://www.icases.ru/upload/iblock/e64/e648b4836083b74445ce81169b209968.jpg');
+});
+
+
+$('#green-m').click(function(){
+	$('.png').attr('src', 'https://www.icases.ru/upload/iblock/78d/78d3b733b08fd4db8ce37a05ae00d7d2.jpg');
+});
